aebb4bd8e43ca0154ab8c49a899d0b006c72ca1a
Danylo Voloshyn <danilovoloshyn@gmail.com>
2023-12-26 14:28:52 +0200

    New project

---
+++ Second_Homework/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Second_Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
